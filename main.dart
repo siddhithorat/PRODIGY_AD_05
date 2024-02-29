66497d9e0b309f92192bc175f82837b194115829
SIDDHI THORAT <siddhit20@gmail.com>
2024-03-01 02:45:03 +0530

    main.dart

---
+++ main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class 
+ extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String result = "Hello World...!";
+  Future _scanQR() async {
+    try {
+      String? cameraScanResult = await scanner.scan();
+      setState(() {
+        result =
+            cameraScanResult!; // setting string result with cameraScanResult
+      });
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner Example In Flutter"),
+      ),
+      body: Center(
+        child: Text(result), // Here the scanned result will be shown
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          icon: Icon(Icons.camera_alt),
+          onPressed: () {
+            _scanQR(); // calling a function when user click on button
+          },
+          label: Text("Scan")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
